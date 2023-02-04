cda94e64f7ba629837249b1edc9eb3948423a010
nursalimdev <nursalimdev123@gmail.com>
2023-02-04 15:51:10 +0700

    commit

--- src/test/java/nursalim/dev/i18n/ResourceBundleTest.java
+++ src/test/java/nursalim/dev/i18n/ResourceBundleTest.java
@@ -2,6 +2,7 @@ package nursalim.dev.i18n;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ResourceBundleTest {
@@ -10,5 +11,28 @@ public class ResourceBundleTest {
         var resourceBundle = ResourceBundle.getBundle("message");
         System.out.println(resourceBundle.getString("hello"));
         System.out.println(resourceBundle.getString("goodbye"));
+    }
+
+    @Test
+    void testResourceBundleIndonesia(){
+        var resourceBundle = ResourceBundle.getBundle("message", new Locale("id", "ID"));
+        System.out.println(resourceBundle.getString("hello"));
+        System.out.println(resourceBundle.getString("goodbye"));
+    }
+
+    @Test
+    void testResourceBundleNotFound(){
+        var resourceBundle = ResourceBundle.getBundle("message", new Locale("en", "US"));
+        System.out.println(resourceBundle.getString("hello"));
+        System.out.println(resourceBundle.getString("goodbye"));
+    }
+
+    @Test
+    void testResourceBundleMultipleTimes(){
+        var resourceBundle1 = ResourceBundle.getBundle("message", new Locale("en", "US"));
+        var resourceBundle2 = ResourceBundle.getBundle("message", new Locale("en", "US"));
+
+        System.out.println(resourceBundle1.equals(resourceBundle2));
+        System.out.println(resourceBundle1 == resourceBundle2);
     }
 }
